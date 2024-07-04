e86c34eebe1c2bb43b03e4eaddbb7f2cde998f10
vova <hohiop09@gmail.com>
2024-07-04 20:09:50 +0300

    added favicon and correct logic with cart

--- src/components/ui/ActiveIcons/Cart/Cart.tsx
+++ src/components/ui/ActiveIcons/Cart/Cart.tsx
@@ -4,13 +4,14 @@ import { StateContext } from 'src/store';
 
 const Cart = () => {
   const { cart } = useContext(StateContext);
-  const isCart = cart.length > 0;
+  const ItemsInCart = cart.reduce((a, b) => a + b.amount, 0);
+  const isCart = ItemsInCart > 0;
 
   return (
     <div className="cart">
       <div className="cart__icon">
         <img src="icons/Cart.png" alt="cart" className="cart__icon--img" />
-        {isCart && <div className="cart__icon--counter">{cart.length}</div>}
+        {isCart && <div className="cart__icon--counter">{ItemsInCart}</div>}
       </div>
     </div>
   );
